9eee206e5eb0016a7ef020c493b0a6498a908237
Oksana <maximovaoxana@gmail.com>
2020-04-28 22:36:27 +0300

    add server part

--- server/app.js
+++ server/app.js
@@ -1,57 +1,23 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const app = express()
-// const mongoose = require('mongoose');
-// const url = 'mongodb://localhost/blogDb';
+const mongoose = require('mongoose');
 
-// const User = require('./model/user');
+const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended : false}))
 
-app.get('/', (req, res) => {
-	res.send('Hello World!')
+const url = 'mongodb://localhost/blogDb'; //todo: move to config
+
+app.get('/api/user/login', (req, res) => {
+    res.send('Hello World!')
 })
 
-// app.post('/api/user/login', (req, res) => {
-// 	mongoose.connect(url,{ useMongoClient: true }, function(err){
-// 		if(err) throw err;
-// 		User.find({
-// 			username : req.body.username, password : req.body.password
-// 		}, function(err, user){
-// 			if(err) throw err;
-// 			if(user.length === 1){
-// 				return res.status(200).json({
-// 					status: 'success',
-// 					data: user
-// 				})
-// 			} else {
-// 				return res.status(200).json({
-// 					status: 'fail',
-// 					message: 'Login Failed'
-// 				})
-// 			}
-//
-// 		})
-// 	});
-// })
-//
-// app.post('/api/user/create', (req, res) => {
-// 	mongoose.connect(url, function(err){
-// 		if(err) throw err;
-// 		const user = new User({
-// 			name: req.body.name,
-// 			username: req.body.username,
-// 			password: req.body.password
-// 		})
-// 		user.save((err, res) => {
-// 			if(err) throw err;
-// 			return res.status(200).json({
-// 				status: 'success',
-// 				data: res
-// 			})
-// 		})
-// 	});
-// })
+app.post('/api/user/login', (req, res) => {
+    mongoose.connect(url, function(err){
+        if(err) throw err;
+        console.log('connected successfully, username is ',req.body.username,' password is ',req.body.password);
+    });
+})
 
-app.listen(3000, () => console.log('Blog server running on port 3000!'))
+app.listen(3000, () => console.log('blog server running on port 3000!'))
