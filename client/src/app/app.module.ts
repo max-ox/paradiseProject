a109a1fbfd72f60cd8d508f7bfed48a7cccc3d57
Oksana <maximovaoxana@gmail.com>
2020-04-28 22:27:45 +0300

    some upd

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+        { path: '', pathMatch: 'full', redirectTo: 'welcome' },
+        { path: 'login', component: LoginComponent },
+        { path: 'welcome', component: HomeComponent }
+        ]
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
