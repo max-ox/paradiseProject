32b175b75c004b9328fc6463fd755755016ed998
Oksana <maximovaoxana@gmail.com>
2020-05-04 10:24:30 +0300

    add report model

--- server/models/report.js
+++ server/models/report.js
@@ -2,23 +2,23 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
-var UserSchema = new Schema(
+var ReportSchema = new Schema(
     {
         name: {type: String, required: true, max: 200},
         scoreOP: {type: Number, required: true, min: 0, max: 10},
         scoreVP: {type: Number, required: true, min: 0, max: 500},
         doc: {type: String, required: true, min: 1500},
         is_spec_survive: {type: Boolean, required: true},
+        number_of_survivors: {type: Number, required: true, min: 0, max: 50},
+        varcor_level: {type: Number, required: true, min: 0, max: 50},
+        armicode: {type: String, required: true, max: 100},
+        status: {type: String, required: true, max: 100},
+        is_problem: {type: Boolean, required: true},
+        author: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
+        opponent: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
+        mission: {type: mongoose.Schema.Types.ObjectId, ref: "Mission"}
     }
 );
 
-
-// Виртуальное свойство - URL автора
-// UserSchema
-//     .virtual('url')
-//     .get(function () {
-//         return '/catalog/author/' + this._id;
-//     });
-
 //Export model
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('Report', ReportSchema);
