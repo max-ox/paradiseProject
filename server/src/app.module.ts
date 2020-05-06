f224149834577e967700d99839045fd663a31b4c
Oksana <maximovaoxana@gmail.com>
2020-05-06 21:11:54 +0300

    add passport

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,12 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 // var config = require('./config');
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/paradiseDB', { useNewUrlParser: true }),
     UserModule,
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
