fd38ed934d2e69836ea3993c9dfb5ce52e51a000
Oksana <maximovaoxana@gmail.com>
2020-05-08 15:17:44 +0300

    return user profile after login

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
+import { User } from '../interfaces/user.interface';
 import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
@@ -12,14 +13,13 @@ export class AuthService {
     async validateUser(username: string, pass: string): Promise<any> {
         const user = await this.usersService.findOne(username);
         if (user && user.password === pass) {
-            const { password, ...result } = user;
-            return result;
+            return user;
         }
         return null;
     }
 
-    async login(user: any) {
-        const payload = { username: user.username, sub: user.userId };
+    async login(user: User) {
+        const payload = { email: user.email, _id: user._id };
         return {
             access_token: this.jwtService.sign(payload),
         };
